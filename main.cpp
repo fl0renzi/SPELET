00928b04c6c9f7471c8564adef9e2ade7b810c3d
Johan <elvemoon@gmail.com>
2020-07-01 18:24:12 +0200

    test

--- main.cpp
+++ main.cpp
@@ -19,8 +19,7 @@ void numbersgame() {
     random = rand() % (maxnumber - minnumber + 1) + minnumber; // Funktion för att slumpmässigt genera ett tal 1-36
     cout << "V\x84lj en siffra mellan 1-36 :" << endl;
     cin >> number;
-    cout << "Siffran \x84r ";
-    cout << random;
+    cout << "Siffran \x84r "; cout << random;
     cout << "\n";
 
     // IF-sats som kollar ifall spelaren har vunnit samt ökar kassa med 10ggr pengarna
@@ -60,6 +59,7 @@ void blacknumber() // Funktion för jämna nummer
 
     cout << "Siffran \x84r ";
 
+// Kollar ifall siffrarn är ett jämt nummer vilket i detta spel resulterar i en vinst
     if (random % 2 == 0) {
 
         cout << random << " vilket \x84r ett j\x84mt nummer";
@@ -69,7 +69,7 @@ void blacknumber() // Funktion för jämna nummer
         cout << "Du vann ";
         cout << bets * 2;
         cout << "kr" << endl;
-
+// Om siffran inte är ett jämt nummer så måste siffran vara ojämn
     } else {
         cout << random << " vilket \x84r ett oj\x84mt nummer";
         cout << "\n";
@@ -79,6 +79,7 @@ void blacknumber() // Funktion för jämna nummer
         cout << bets;
         cout << "kr" << endl;
     }
+    // Kontroll fråga om användaren vill forsätta spela annars stängs progammet ner
     cout << "Vill du spela igen? j/n ";
     cin >> val;
     if (val == "j") {
@@ -91,9 +92,11 @@ void blacknumber() // Funktion för jämna nummer
 
 void rednumber() // Funktion för ojämna nummer
 {
+    // Funktion för att genera ett nummer mellan 1-36
     srand(time(nullptr));
     random = rand() % (maxnumber - minnumber + 1) + minnumber;
     cout << "Siffran \x84r ";
+   // Kollar ifall siffrarn är ett jämt nummer vilket i detta spel resulterar i en förlust
     if (random % 2 == 0) {
         cout << random << " vilket \x84r ett j\x84mt nummer";
         cout << "\n";
@@ -102,6 +105,7 @@ void rednumber() // Funktion för ojämna nummer
         cout << "Du f\x94lora ";
         cout << bets;
         cout << "kr" << endl;
+      //  Om siffran inte är ett jämt nummer så måste siffran vara ojämn vilket ger en vinst
     } else {
         cout << random << " vilket \x84r ett oj\x84mt nummer";
         cout << "\n";
@@ -111,6 +115,7 @@ void rednumber() // Funktion för ojämna nummer
         cout << bets * 2;
         cout << "kr" << endl;
     }
+    // Kontroll fråga om användaren vill forsätta spela annars stängs progammet ner
     cout << "Vill du spela igen? j/n ";
     cin >> val;
     if (val == "j") {
@@ -124,23 +129,30 @@ void rednumber() // Funktion för ojämna nummer
 
 int main() {
     {
-
+        // Välkomsmeddelande för användaren som bara visar spelet en gång
         cout << "<<<--- V\x84lkommen till Roulette spelet ! --->>>" << endl;
+        cout << "<<<--- Gjort av Johan Elvemo version 0.1 pre alpha --->>>" << endl;
+        // While loop som gör att spelaren kan spela flera gånger
         while (continueplay) {
+          // Kontroll för att kolla ifall användaren har några pengar kvar
             if (startingmoney <= 0) {
                 cout << "Slut p\x86 pengar!";
                 exit(0); //
             }
+        // Användaren placerar ett bet
             cout << "Du har " << startingmoney;
             cout << "kr kvar att spela f\x94r" << endl;
             cout << "Placera ett bet: 100,300 eller 500 " << endl;
             cin >> bets;
+        // Kollar ifall bettet är större än vad användaren har i kassan    
             if (bets > startingmoney) {
 
                 cout << "Du har inte cash for det" << endl;
                 cout << "Avslutar programmet for din skull";
                 exit(0);
+                
             } else {
+                // Kontrollerar att bettet är 100,300 eller 500kr och programmet fortsätter inte förräns rätt summa är anvigen
                 while (bets != 100 && bets != 300 && bets != 500) {
                     cout << "Du m\x86ste ange 100, 300 eller 500kr " << endl;
                     cout << "F\x94rsok igen" << endl;
@@ -148,7 +160,9 @@ int main() {
                     cin >> bets;
                 }
             }
-
+            /* Meny för att bestämma vilken typ av spel som användaren vill spela
+               Går även att avsluta programmet från denna meny
+               */
             cout << endl
                  << "1 - Straight up Ett singelspel p\x86 valfritt nummer.\n"
                  << " 2 - Spela p\x86 j\x84mna nummer - Svart. \n"
@@ -158,7 +172,7 @@ int main() {
             cout << " och bekrafta med Enter";
             cout << "\n";
 
-
+            // När användare har gjort ett val går det till denna Swtich meny som anropar olika funktion för de olika spelen
             int menyval;
             cin >> menyval;
 
